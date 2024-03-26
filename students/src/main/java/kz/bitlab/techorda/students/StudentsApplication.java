318f152816036d887354532e7e0645fa84ca9e25
Kulashh <k.bazarbayeva@list.ru>
2024-03-27 03:13:10 +0500

    Spring Sprint task1

---
+++ students/src/main/java/kz/bitlab/techorda/students/StudentsApplication.java
@@ -0,0 +1,14 @@
+package kz.bitlab.techorda.students;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentsApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(StudentsApplication.class, args);
+	}
+
+}
